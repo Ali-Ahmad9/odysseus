71fe9949963ba377e3784e9d8e484f6b4c3e8ced
Ali-Ahmad9 <ahmadalix@outlook.com>
2020-06-05 20:02:05 +0000

    Sending email with live ticker prices

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import win32com.client as win32
 from lib.constants import Constants
+from yahoo_fin import stock_info
+
+
+TICKERS = [
+    'AAPL',
+    'AAL',
+    'MCX'
+]
+
+
+def get_live_prices():
+    return [(ticker, stock_info.get_live_price(ticker)) for ticker in TICKERS]
+
 
 def send_email():
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = Constants.EMAIL
     mail.Subject = 'Test'
-    mail.HTMLBody = '<h2>This is a test</h2>'  # this field is optional
+    mail.Body = str(get_live_prices())
     mail.Send()
 
 
-if __name__ == '__main__':
+def main():
     send_email()
+
+
+if __name__ == '__main__':
+    main()
