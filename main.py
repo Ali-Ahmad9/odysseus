d4cdb9ada4eb16e7b5cf53f7e7960f5b48b4d4c1
Ali-Ahmad9 <ahmadalix@outlook.com>
2020-06-05 10:10:20 +0000

    Added send email functionality

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import win32com.client as win32
+from lib.constants import Constants
+
+def send_email():
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = Constants.EMAIL
+    mail.Subject = 'Test'
+    mail.HTMLBody = '<h2>This is a test</h2>'  # this field is optional
+    mail.Send()
+
+
 if __name__ == '__main__':
-    pass
+    send_email()
